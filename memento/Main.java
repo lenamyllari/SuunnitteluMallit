0f8115f2a019469141ac8ce394f81ef5dbed5e64
Elena Mylläri <43740050+lenamyllari@users.noreply.github.com>
2020-04-10 09:54:59 +0300

    Update Main.java

--- memento/Main.java
+++ memento/Main.java
@@ -1,8 +1,5 @@
 package memento;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -12,19 +9,12 @@ public class Main {
 		Player player3 = new Player(3, riddler);
 		Player player4 = new Player(4, riddler);
 		Player player5 = new Player(5, riddler);
-		List<Player> players = new ArrayList();
-		players.add(player1);
-		players.add(player2);
-		players.add(player3);
-		players.add(player4);
-		players.add(player5);
-		for(Player p : players) {
-			p.joinGame();
-		}
-		for(Player p : players) {
-			System.out.println("\nPlayer number " + p.getId() + " guessing:");
-			p.guess();
-		}
+		
+		player1.run();
+		player2.run();
+		player3.run();
+		player4.run();
+		player5.run();
 	}
 
 }
